fe47b67f5c7a0e2ea0893d0c8d645b952e575573
Gabor Csardi <csardi.gabor@gmail.com>
2016-02-25 23:50:44 +0000

    Unicode characters in printout

--- R/print.R
+++ R/print.R
@@ -1,20 +1,21 @@
 
 #' @export
+#' @importFrom clisymbols symbol
 
 print.rcmdcheck <- function(x, ...) {
 
   if (length(x$errors)) {
-    print_header("Error(s)")
+    print_header(paste(symbol$cross, "Error(s)"))
     lapply(x$errors, print_entry)
   }
 
   if (length(x$warnings)) {
-    print_header("Warning(s)")
+    print_header(paste(symbol$warning, "Warning(s)"))
     lapply(x$warnings, print_entry)
   }
 
   if (length(x$notes)) {
-    print_header("Note(s)")
+    print_header(paset(symbol$info, "Note(s)"))
     lapply(x$notes, print_entry)
   }
 }
@@ -37,7 +38,7 @@ print_header <- function(text) {
 print_entry <- function(entry) {
   lines <- strsplit(entry, "\n", fixed = TRUE)[[1]]
 
-  first <- paste0("* ", lines[1])
+  first <- paste0(symbol$pointer, " ", lines[1])
   cat(red(first), "\n", sep = "")
 
   cat(paste0("  ", lines[-1]), sep = "\n", "")
