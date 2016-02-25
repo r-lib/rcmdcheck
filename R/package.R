cc583a38939639c5ead63ffa82725ea753dcd2cb
Gabor Csardi <csardi.gabor@gmail.com>
2016-02-25 13:14:34 +0000

    Minimal manual

--- R/package.R
+++ R/package.R
@@ -8,7 +8,13 @@
 #' @name rcmdcheck
 NULL
 
-
+#' Run \code{R CMD check} on a package or a directory
+#'
+#' @param path Path to a package tarball or a directory
+#' @return An S3 object (list) with fields \code{errors},
+#'   \code{warnings} and \code{nodes}. These are all character
+#'   vectors containing the output for the failed check.
+#'
 #' @export
 
 rcmdcheck <- function(path) {
