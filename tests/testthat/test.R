56bc9631da67083dd39a9096c3d58b1fed04ac4b
Gabor Csardi <csardi.gabor@gmail.com>
2016-02-25 14:01:42 +0000

    Add a simple test case

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -3,6 +3,7 @@ context("rcmdcheck")
 
 test_that("rcmdcheck works", {
 
-  expect_true(TRUE)
+  bad1 <- rcmdcheck("bad1")
+  expect_match(bad1$warnings[1], "Non-standard license specification")
 
 })
