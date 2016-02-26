a32253ef15b523967e2e567ec7247ee730bc3570
Gabor Csardi <csardi.gabor@gmail.com>
2016-02-26 10:03:30 +0000

    Nicer printout, summary method

--- R/print.R
+++ R/print.R
@@ -10,14 +10,16 @@ print.rcmdcheck <- function(x, ...) {
   }
 
   if (length(x$warnings)) {
-    print_header(paste(symbol$warning, "Warning(s)"))
+    print_header(paste("!", "Warning(s)"))
     lapply(x$warnings, print_entry)
   }
 
   if (length(x$notes)) {
-    print_header(paset(symbol$info, "Note(s)"))
+    print_header(paste(symbol$info, "Note(s)"))
     lapply(x$notes, print_entry)
   }
+
+  summary(x, ...)
 }
 
 #' @importFrom crayon magenta
@@ -33,13 +35,46 @@ print_header <- function(text) {
   cat(magenta(str), "\n\n", sep = "")
 }
 
-#' @importFrom crayon red
+#' @importFrom crayon red make_style
 
 print_entry <- function(entry) {
+
+  greyish <- make_style("darkgrey")
+
   lines <- strsplit(entry, "\n", fixed = TRUE)[[1]]
 
   first <- paste0(symbol$pointer, " ", lines[1])
   cat(red(first), "\n", sep = "")
 
-  cat(paste0("  ", lines[-1]), sep = "\n", "")
+  cat(paste0("  ", greyish(lines[-1])), sep = "\n", "")
+}
+
+#' @export
+
+summary.rcmdcheck <- function(object, ...) {
+
+  cat("-- ")
+  summary_entry(object, "errors")
+  cat(" | ")
+  summary_entry(object, "warnings")
+  cat(" | ")
+  summary_entry(object, "notes")
+  cat("\n")
+}
+
+#' @importFrom crayon green red
+
+summary_entry <- function(x, name) {
+
+  len <- length(x[[name]])
+
+  if (len == 0) {
+    cat(green(paste(len, name, symbol$tick)))
+
+  } else if (len == 1) {
+    cat(red(paste(len, name, symbol$cross)))
+
+  } else {
+    cat(red(paste(len, name, symbol$cross)))
+  }
 }
