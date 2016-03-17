c45ab2a98750dd4bb54290a3b65da13fd905cad5
Gabor Csardi <csardi.gabor@gmail.com>
2016-03-17 14:45:07 +0000

    Test non-quiet mode as well

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -13,3 +13,26 @@ test_that("rcmdcheck works", {
     "Non-standard license specification"
   )
 })
+
+test_that("non-quiet mode works", {
+
+  Sys.unsetenv("R_TESTS")
+
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+
+  sink(tmp)
+
+  bad1 <- rcmdcheck("bad1", quiet = FALSE)
+  expect_match(bad1$warnings[1], "Non-standard license specification")
+
+  expect_output(
+    print(bad1),
+    "Non-standard license specification"
+  )
+
+  sink(NULL)
+
+  out <- read_char(tmp)
+  expect_match(out, "Non-standard license specification")
+})
