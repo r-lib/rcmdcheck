5a67924a95f9987779509d0a89bbcdb51f5ccdd6
Gabor Csardi <csardi.gabor@gmail.com>
2016-04-13 09:20:19 +0100

    libpath and repos options with sensible defaults

--- R/package.R
+++ R/package.R
@@ -14,6 +14,11 @@ NULL
 #' @param quiet Whether to print check output during checking.
 #' @param args Character vector of arguments to pass to
 #'   \code{R CMD check}.
+#' @param libpath The library path to set for the check.
+#'   The default uses the current library path.
+#' @param repos The \code{repos} option to set for the check.
+#'   This is needed for cyclic dependency checks if you use the
+#'   \code{--as-cran} argument. The default uses the current value.
 #' @return An S3 object (list) with fields \code{errors},
 #'   \code{warnings} and \code{nodes}. These are all character
 #'   vectors containing the output for the failed check.
@@ -21,14 +26,21 @@ NULL
 #' @export
 #' @importFrom withr with_dir
 
-rcmdcheck <- function(path = ".", quiet = FALSE, args = character()) {
+rcmdcheck <- function(path = ".", quiet = FALSE, args = character(),
+                      libpath = .libPaths(), repos = getOption("repos")) {
 
   targz <- build_package(path)
   on.exit(unlink(dirname(targz), recursive = TRUE), add = TRUE)
 
   with_dir(
     dirname(targz),
-    out <- safe_check_packages(basename(targz), args = args, quiet = quiet)
+    out <- safe_check_packages(
+      basename(targz),
+      args = args,
+      quiet = quiet,
+      libpath = libpath,
+      repos = repos
+    )
   )
 
   invisible(parse_check_output(out))
--- R/safe_check.R
+++ R/safe_check.R
@@ -1,13 +1,16 @@
 
-safe_check_packages <- function(..., args, quiet) {
+safe_check_packages <- function(..., args, quiet, libpath, repos) {
 
-  lib <- paste(.libPaths(), collapse = .Platform$path.sep)
+  lib <- paste(libpath, collapse = .Platform$path.sep)
+
+  profile <- tempfile()
+  cat("options(repos=", deparse(repos), ")\n", sep = "", file = profile)
 
   with_envvar(
     c(R_LIBS = lib,
       R_LIBS_USER = lib,
       R_LIBS_SITE = lib,
-      R_PROFILE_USER = tempfile()),
+      R_PROFILE_USER = profile),
     R("CMD", "check", ..., args, callback = if (!quiet) check_callback())
   )
 }
