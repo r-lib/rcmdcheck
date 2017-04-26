0b9b5318678879cb50a954a5745c555a80ce56f5
Gábor Csárdi <csardi.gabor@gmail.com>
2017-04-26 19:47:46 +0100

    Run R CMD check in the background

--- R/background.R
+++ R/background.R
@@ -1,17 +1,27 @@
 
-#' Start running `R CMD check` in the background
-#'
-#' @inheritParams rcmdcheck
-#' @return A list with two elements:
-#'   * `process` is the [processx::process] object of the background process.
-#'   * `targz` is the (temporary) `tar.gz` package file.
-#'
-#' @export
-#' @importFrom callr rcmd_bg_safe
+#' @importFrom R6 R6Class
+NULL
 
-rcmdcheck_bg <- function(path = ".", args = character(),
-                         libpath = .libPaths(),
-                         repos = getOption("repos")) {
+#' @export
+
+rcmdcheck_process <- R6Class(
+  "rcmdcheck_process",
+  inherit = callr::rcmd_process,
+  public = list(
+    initialize = function(path = ".", args = character(),
+      libpath = .libPaths(), repos = getOption("repos"))
+      rcc_init(self, private, super, path, args, libpath, repos)
+  ),
+  private = list(
+    path  = NULL,
+    tmp   = NULL,
+    targz = NULL
+  )
+)
+
+#' @importFrom callr rcmd_process rcmd_process_options
+
+rcc_init <- function(self, private, super, path, args, libpath, repos) {
 
   if (file.info(path)$isdir) {
     path <- find_package_root_file(path = path)
@@ -21,15 +31,21 @@ rcmdcheck_bg <- function(path = ".", args = character(),
 
   targz <- build_package(path, tmp <- tempfile())
 
-  proc <- with_dir(
-    dirname(targz),
-    rcmd_bg_safe(
-      "check",
-      cmdargs = c(basename(targz), args),
-      libpath = libpath,
-      repos = repos
-    )
+  private$path  <- path
+  private$tmp   <- tmp
+  private$targz <- targz
+
+  options <- rcmd_process_options(
+    cmd = "check",
+    cmdargs = c(basename(targz), args),
+    libpath = libpath,
+    repos = repos
   )
 
-  list(process = proc, targz = targz)
+  with_dir(
+    dirname(targz),
+    super$initialize(options)
+  )
+
+  invisible(self)
 }
