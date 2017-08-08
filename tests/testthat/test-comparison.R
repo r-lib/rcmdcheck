204b48db936eca327c5c981d85b1efe3d0509665
hadley <h.wickham@gmail.com>
2017-08-08 13:44:15 -0500

    Add printing regression test

--- tests/testthat/test-comparison.R
+++ tests/testthat/test-comparison.R
@@ -6,3 +6,14 @@ test_that("basic metadata stored in comparison object", {
   expect_equal(cf$package, "REDCapR")
   expect_equal(cf$versions, c("0.9.8", "0.9.8"))
 })
+
+test_that("print message displays informative output", {
+  cf <- compare_check_files(test_path("minimal-ee.log"), test_path("minimal-ewn.log"))
+
+  expect_output_file({
+    print(summary(cf))
+    cat("\n\n")
+    print(cf)
+  }, file = "comparison-newly-failing.txt", update = TRUE)
+
+})
