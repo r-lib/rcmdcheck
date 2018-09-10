5b964c75d3cce0714fbd1ddd9110fbddde88bda2
Gábor Csárdi <csardi.gabor@gmail.com>
2018-09-10 23:06:44 +0100

    Make R CMD build output look better
    We can use the same callbacks as for check, but
    need to run the build in the bg for this, as pkgbuild::build
    does not expose the callr::rcmd arguments.
    
    Closes #81.

--- R/build.R
+++ R/build.R
@@ -1,5 +1,5 @@
 
-#' @importFrom pkgbuild build
+#' @importFrom pkgbuild pkgbuild_process
 #' @importFrom withr with_envvar
 
 build_package <- function(path, tmpdir, build_args, libpath, quiet) {
@@ -8,8 +8,23 @@ build_package <- function(path, tmpdir, build_args, libpath, quiet) {
   if (file.info(path)$isdir) {
     if (!quiet) cat_head("R CMD build")
     with_envvar(
-      c("R_LIBS_USER" = paste(libpath, collapse = .Platform$path.sep)),
-      build(path, tmpdir, args = build_args, quiet = quiet)
+      c("R_LIBS_USER" = paste(libpath, collapse = .Platform$path.sep)), {
+        proc <- pkgbuild_process$new(path, tmpdir, args = build_args)
+        on.exit(proc$kill(), add = TRUE)
+        callback <- block_callback()
+        while (proc$is_incomplete_output() || proc$is_incomplete_error()) {
+          proc$poll_io(-1)
+          out <- proc$read_output()
+          err <- proc$read_error()
+          if (!quiet) {
+            out <- sub("(checking for file .)/.*DESCRIPTION(.)",
+                       "\\1.../DESCRIPTION\\2", out, perl = TRUE)
+            callback(out)
+            callback(err)
+          }
+        }
+        proc$get_built_file()
+      }
     )
 
   } else {
