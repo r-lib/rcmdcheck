ad38de2322cc2adca0c12d94cea286280f6d7560
Gábor Csárdi <csardi.gabor@gmail.com>
2019-05-06 22:25:44 +0100

    Use another CRAN test
    The original one was missing.

--- tests/testthat/test-cran.R
+++ tests/testthat/test-cran.R
@@ -3,7 +3,7 @@ context("compare_to_cran")
 
 test_that("can get results (windows)", {
   skip_on_cran()
-  xx <- cran_check_results("rcmdcheck", "r-devel-windows-ix86+x86_64")
+  xx <- cran_check_results("rcmdcheck", "r-release-windows-ix86+x86_64")
   expect_s3_class(xx[[1]], "rcmdcheck")
 })
 
