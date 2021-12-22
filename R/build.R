4d18029e02b9e3dee0ae06ec81c6c5eb932bcf33
Michael Chirico <michaelchirico4@gmail.com>
2021-12-21 16:35:44 -0800

    fix typos

--- R/build.R
+++ R/build.R
@@ -61,7 +61,7 @@ check_for_tilde_file <- function(path) {
       "This package contains a file or directory named `~`. ",
       "Because of a bug in older R versions (before R 4.0.0), ",
       "building this package might delete your entire home directory!",
-      "It is best to (carefully!) remove the file. rcmdcehck will exit now."
+      "It is best to (carefully!) remove the file. rcmdcheck will exit now."
     )
   }
 }
