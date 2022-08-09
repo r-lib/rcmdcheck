6d1fb501d3aa18d9523d2c53cbb0ed6c6689dcb3
fweber144 <fweber144@protonmail.com>
2022-08-09 06:55:15 +0200

    Fix docs for argument `check_dir` of `rcmdcheck()` (omitting "not").

--- R/package.R
+++ R/package.R
@@ -77,9 +77,9 @@ NULL
 #'   spaces to delimit arguments like you would in the shell). For example,
 #'   `build_args = c("--force", "--keep-empty-dirs")` is a correct usage and
 #'   `build_args = "--force --keep-empty-dirs"` is incorrect.
-#' @param check_dir Path to a directory where the check is performed.
-#'   If this is not `NULL`, then a temporary directory is used, that
-#'   is cleaned up when the returned object is garbage collected.
+#' @param check_dir Path to a directory where the check is performed. If this is
+#'   `NULL`, then a temporary directory is used, that is cleaned up when the
+#'   returned object is garbage collected.
 #' @param libpath The library path to set for the check.
 #'   The default uses the current library path.
 #' @param repos The `repos` option to set for the check.
