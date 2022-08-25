d1bf54244c959ee9625c2c9379a53e17e9e08ece
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-08-25 08:35:54 +0200

    Test for `cran_check_flavours()`

--- tests/testthat/test-cran.R
+++ tests/testthat/test-cran.R
@@ -15,11 +15,17 @@ test_that("can get results (windows)", {
 test_that("can get UTF-8 results", {
   skip_on_cran()
   xx <- cran_check_results("rcmdcheck", "r-devel-linux-x86_64-debian-gcc")
-  expect_s3_class(xx[[1]], "rcmdcheck")  
+  expect_s3_class(xx[[1]], "rcmdcheck")
 })
 
 test_that("can get ISO-8859-15 results", {
   skip_on_cran()
   xx <- cran_check_results("rcmdcheck", "r-devel-linux-x86_64-debian-clang")
-  expect_s3_class(xx[[1]], "rcmdcheck")  
+  expect_s3_class(xx[[1]], "rcmdcheck")
+})
+
+test_that("when package unspecified, cran check flavours are returned", {
+  flavours <- cran_check_flavours()
+
+  expect_true(all(grepl("r-devel|r-patched|r-release|r-oldrel", flavours)))
 })
