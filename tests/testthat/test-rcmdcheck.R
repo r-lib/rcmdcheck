dac426e1bf4ee5857ae290835dd9c3019cb18267
Hadley Wickham <h.wickham@gmail.com>
2026-09-17 14:58:42 -0500

    Muffle pdflatex message in build arguments snapshot test

--- tests/testthat/test-rcmdcheck.R
+++ tests/testthat/test-rcmdcheck.R
@@ -166,12 +166,21 @@ test_that("build arguments", {
   tmp <- tempfile()
   on.exit(unlink(tmp), add = TRUE)
 
+  # pdflatex availability differs across machines, so muffle its message
+  check_bad1_verbose <- function() {
+    withCallingHandlers(
+      rcmdcheck(test_path("bad1"), build_args = "-v"),
+      message = function(m) {
+        if (grepl("pdflatex not found", conditionMessage(m))) {
+          invokeRestart("muffleMessage")
+        }
+      }
+    )
+  }
   expect_snapshot(
     error = TRUE,
-    rcmdcheck(test_path("bad1"), build_args = "-v"),
+    check_bad1_verbose(),
     transform = function(x) {
-      # pdflatex availability differs across machines
-      x <- x[!grepl("pdflatex not found", x)]
       x <- sub(
         "package builder: [.0-9]+ [(]r[0-9]+[)]",
         "package builder: <rvesion> (r<commit>)",
